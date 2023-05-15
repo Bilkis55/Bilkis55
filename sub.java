7fa18f193425f54f7312d673bed370210d68f9a2
Bilkis55 <133634469+Bilkis55@users.noreply.github.com>
2023-05-15 16:58:39 +0530

    this file contains my details

---
+++ sub.java
@@ -0,0 +1,17 @@
+public class  sub{
+
+public static void main(String[]args)
+{
+
+System.out.println("Name :- Bilkis Jaleel Sayyad");
+
+System.out.println("ph:-9370160011");
+
+System.out.println("Add:-Pune");
+
+System.out.println("Hobby:-chess");
+
+System.out.println("Home Town :- Latur");
+ 
+}
+}
